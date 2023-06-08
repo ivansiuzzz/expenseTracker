8135442fbcac5c32a1579ecd3c7e0e343dcd8643
ivansiuzzzzz <ivan.siu@icloud.com>
2023-06-09 02:07:32 +0800

    add textfield in add expense model

--- lib/widgets/expenses.dart
+++ lib/widgets/expenses.dart
@@ -1,5 +1,6 @@
 import 'package:expensetracker/widgets/expenses_list/expenses_list.dart';
 import 'package:expensetracker/model/Expense.dart';
+import 'package:expensetracker/widgets/new_expense.dart';
 import 'package:flutter/material.dart';
 
 class Expenses extends StatefulWidget {
@@ -23,12 +24,18 @@ class _ExpensesState extends State<Expenses> {
         category: Category.food),
   ];
 
+  void _openAddExpenseOverlay() {
+    showModalBottomSheet(context: context, builder: (ctx) => NewExpense());
+  }
+
   @override
   Widget build(BuildContext context) {
     return Scaffold(
       appBar: AppBar(
         title: Text("Learning Flutter ^^"),
-        actions: [IconButton(onPressed: () {}, icon: Icon(Icons.add))],
+        actions: [
+          IconButton(onPressed: _openAddExpenseOverlay, icon: Icon(Icons.add))
+        ],
       ),
       body: Column(
         children: [
