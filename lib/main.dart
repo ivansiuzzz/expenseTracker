0fde53cd381529cd5d39561252fbacaf4188e50b
ivansiuzzzzz <ivan.siu@icloud.com>
2023-06-11 22:45:03 +0800

    finally tidy up code

--- lib/main.dart
+++ lib/main.dart
@@ -1,6 +1,8 @@
 import 'package:expensetracker/widgets/expenses.dart';
 import 'package:flutter/material.dart';
 
+var kColorScheme = const Color.fromARGB(255, 59, 181, 181);
+
 void main() {
   runApp(const MyApp());
 }
@@ -17,6 +19,13 @@ class MyApp extends StatelessWidget {
       theme: ThemeData(
         colorScheme: ColorScheme.fromSeed(seedColor: Colors.deepPurple),
         useMaterial3: true,
+        appBarTheme: AppBarTheme().copyWith(
+            backgroundColor: kColorScheme, foregroundColor: Colors.white),
+        cardTheme: CardTheme().copyWith(
+            color: Colors.blueGrey,
+            margin: EdgeInsets.symmetric(horizontal: 16, vertical: 8)),
+        elevatedButtonTheme: ElevatedButtonThemeData(
+            style: ElevatedButton.styleFrom(backgroundColor: kColorScheme)),
       ),
       home: Expenses(),
     );
